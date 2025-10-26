eaed49be95e3bd428996d750f29f5b7e4ad87665
poornima-ravilla <poornimaravilla@gmail.com>
2025-10-26 09:51:11 +0530

    Updated firebase.js to use environment variables

--- src/firebase.js
+++ src/firebase.js
@@ -5,13 +5,13 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore, doc, setDoc, deleteDoc } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDjWkJKATmAQPhqoRLOjinlCS8cvh9gbM8",
-  authDomain: "cashtrail-ce1d4.firebaseapp.com",
-  projectId: "cashtrail-ce1d4",
-  storageBucket: "cashtrail-ce1d4.firebasestorage.app",
-  messagingSenderId: "780200607400",
-  appId: "1:780200607400:web:502e27cfa21b3421cc4c9d",
-  measurementId: "G-0J2VJEV08Q",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
