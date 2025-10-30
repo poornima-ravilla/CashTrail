63dd9af3fa844e8c12585d4421bb0d6aac96002e
poornima-ravilla <poornimaravilla@gmail.com>
2025-10-30 11:41:50 +0530

    Updated expense category

--- src/components/Modals/addExpense.js
+++ src/components/Modals/addExpense.js
@@ -76,12 +76,12 @@ function AddExpenseModal({
           ]}
         >
           <Select className="select-input-2">
-            <Select.Option value="food">Bills</Select.Option>
-            <Select.Option value="education">Food</Select.Option>
-            <Select.Option value="office">Transport</Select.Option>
-            <Select.Option value="food">Health</Select.Option>
+            <Select.Option value="biils">Bills</Select.Option>
+            <Select.Option value="food">Food</Select.Option>
+            <Select.Option value="transport">Transport</Select.Option>
+            <Select.Option value="health">Health</Select.Option>
             <Select.Option value="education">Education</Select.Option>
-            <Select.Option value="office">Shopping</Select.Option>
+            <Select.Option value="shopping">Shopping</Select.Option>
             {/*Add more tags here*/}
           </Select>
         </Form.Item>
